7f281ca3b344d06abefee998c322d9bb870e84a1
Karen nakayama <knakayama@vassar.edu>
2019-12-25 08:07:21 +0900

    Update 3-model-selection.R

--- 3-model-selection.R
+++ 3-model-selection.R
@@ -24,10 +24,41 @@
 # we use to evaluate which model is a better descriptor of the data for the experiment?
 # Describe briefly how you might make this evaluation.
 
-Test.1<-random.walk.model(1000, drift= 1, sdrw = 0.3, criterion=3)
-sum(Test.1$correct) / length(Test.1$correct)
-mean(Test.1$rt)
+Test.1<-random.walk.model(1000, drift= 0.5, sdrw = 0.3, criterion=4)
+sum(Test.1$correct) / length(Test.1$correct) 
+mean(Test.1$rt) 
 
-Test.2<-accumulator.model(1000)
+Test.2<-accumulator.model(1000, rate.1=50, rate.2 = 55, criterion = 3)
 sum(Test.2$correct) / length(Test.2$correct)
 mean(Test.2$rt)
+
+#Josh's code
+rw.model.result <- random.walk.model(1000, drift=0.012, sdrw=0.3, criterion = 4.8)
+rw.model.result %>% group_by(correct) %>% summarize(mean.rt = mean(rt))
+mean(rw.model.result$correct) #random.walk.model produced a slightly worse result than Josh's earlier, which
+#explains why the results are very different from Josh's with the same drift and criterion.
+
+rw.model.result <- random.walk.model(1000, drift=5, sdrw=0.3, criterion = 1)
+rw.model.result %>% group_by(correct) %>% summarize(mean.rt = mean(rt))
+mean(rw.model.result$correct) #whatever the change in drift or criterion, the rt and accuracy can't
+#get high enough. With my random.walk.model, accumulator.model is a much better model.
+
+acc.model.result <- accumulator.model(1000, rate.1 = 85, rate.2 = 91, criterion=3)
+acc.model.result %>% group_by(correct) %>% summarize(mean.rt = mean(rt))
+mean(acc.model.result$correct)
+
+dev.off()
+layout(matrix(1:4, nrow=2, byrow=T))
+hist((rw.model.result %>% filter(correct==TRUE))$rt, breaks=seq(0,2000,100), main="RW Model, correct", xlab="RT")
+hist((rw.model.result %>% filter(correct==FALSE))$rt, breaks=seq(0,2000,100), main="RW Model, incorrect", xlab="RT")
+hist((acc.model.result %>% filter(correct==TRUE))$rt, breaks=seq(0,2000,10), main="ACC Model, correct", xlab="RT")
+hist((acc.model.result %>% filter(correct==FALSE))$rt, breaks=seq(0,2000,10), main="ACC Model, incorrect", xlab="RT")
+
+#With my version of random.walk.model, both the correct and incorrect are at a similar value on the
+#plot with similar variability and symmetry. It is the same for accumulator model.
+
+rw.model.result %>% group_by(correct) %>% summarize(sd.rt = sd(rt))
+acc.model.result %>% group_by(correct) %>% summarize(sd.rt = sd(rt))
+
+#With my random.walk.model, the SD is around 9 while accumulator model is around 13. But, the RT 
+#and accuracy is nowhere close to the data.
